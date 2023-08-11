26023df6fdd484740076082d304be411fb4f729c
Dumjes <you@example.com>
2023-08-10 21:54:37 -0700

    c solutions

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - the main function is initialized
+ *
+ * Return: this executes to 0
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
