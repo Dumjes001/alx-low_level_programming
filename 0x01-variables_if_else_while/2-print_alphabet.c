7ac81d0ecdcaee55b97ac3a13b743453336fe757
Dumjes <you@example.com>
2023-08-13 14:17:32 -0700

    insomnia

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - main function
+ * main - main fucntion
  *
  * Return: executes to 0
  */
@@ -10,7 +10,7 @@ int main(void)
 {
 	char i;
 
-	for (i = 'a' ; i <= '2' ; i++)
+	for (i = 'a' ; i <= 'z' ; i++)
 		putchar(i);
 	putchar('\n');
 	return (0);
