cf6730040a0a90e5767760d7031d60023d723fb0
Dumjes <you@example.com>
2023-08-13 13:42:24 -0700

    insomnia

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * Return: executes to 0
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 'a' ; i <= '2' ; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
