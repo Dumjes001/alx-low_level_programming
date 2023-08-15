108b266582f6ef84613d9d82bfa2b298f21c011e
Dumjes <you@example.com>
2023-08-15 03:38:11 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * main - the main function
+ *
+ * Return: this executes to 0
+ */
+
+int main(void)
+{
+	char str[] = "_putchar":
+	int ch;
+
+	for (ch = 0; ch < 8; ch++)
+		_putchar(str[chr]);
+	_putchar('\n');
+
+
+	return (0);
+}
+
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,9 @@
+#ifndef MAIN.h
+#define MAIN.h
+
+#include <stdio.h>
+
+int _putchar(char c);
+
+
+#endif
