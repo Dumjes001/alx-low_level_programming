4981180d5cb262867151ceb6e1c51cf9e47dee43
Dumjes <you@example.com>
2023-08-21 14:42:33 -0700

    task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * rev_string - prints the reversed string output to stdout
+ *
+ * @s: this prints the integer
+ * @b - this prints the integer
+ * @i - this print the integer
+ *
+ * Return: Nothing
+ */
+
+void  rev_string(char *s)
+{
+	int b, i;
+	char temp;
+
+	/*find string length without null char*/
+	for (b = 0; s[b] != '\0'; ++b)
+
+	/*swap the string by looping to half the string*/
+	for (i = 0; i < b / 2; i++)
+	{
+		temp = s[i];
+		s[i] = s[b - 1 - i];
+		s[b - 1 - i] = temp;
+	}
+}
