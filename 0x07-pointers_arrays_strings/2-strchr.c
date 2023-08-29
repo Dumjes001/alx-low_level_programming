aa08e3beae4dd20d975b7388cc3fe4199fc9a835
Dumjes <you@example.com>
2023-08-28 17:18:26 -0700

    task 3

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ * _strchr - locates character in a string
+ * @s: string to search
+ * @c: character to searach in s
+ * Return: s or NULL
+ */
+
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			return s;
+		}
+		s++;
+	}
+
+	return NULL;
+}
+
