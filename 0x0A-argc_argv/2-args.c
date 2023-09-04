de9f03aaa1756c077720b1e612d3365e96be441a
Dumjes <you@example.com>
2023-09-04 06:01:46 -0700

    tasak 2

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - print all arguments it receive
+ * @argc: number of command line arguments
+ * @argv: array that contains the program command line arg
+ *
+ * Return: returns 0 on Success
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
