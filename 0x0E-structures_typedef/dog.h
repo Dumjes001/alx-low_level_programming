24734e2eeee87eec77a7e32beb69ed2c821291f9
Dumjes <you@example.com>
2023-09-11 16:57:06 -0700

    task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+ * struct dog - a dog that creates a dog info
+ *
+ * @name: the name of the dog
+ * @age: the age of the dog
+ * @owner: the owner of the dog
+ *
+ * Desc: this creates the struct
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
